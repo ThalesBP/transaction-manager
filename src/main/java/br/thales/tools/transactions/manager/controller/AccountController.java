1256594f974918200fa6108ec89d5aaf029079fe
ThalesBP <thales.pasqual@gmail.com>
2021-07-18 20:15:36 -0300

    Add account apis

--- src/main/java/br/thales/tools/transactions/manager/controller/AccountController.java
+++ src/main/java/br/thales/tools/transactions/manager/controller/AccountController.java
@@ -2,11 +2,21 @@ package br.thales.tools.transactions.manager.controller;
 
 
 import br.thales.tools.transactions.manager.database.AccountRepository;
+import br.thales.tools.transactions.manager.database.UserRepository;
 import br.thales.tools.transactions.manager.model.Account;
+import br.thales.tools.transactions.manager.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.HttpClientErrorException;
 
+import java.util.Date;
 import java.util.List;
+import java.util.Optional;
+
+import static br.thales.tools.transactions.manager.model.Account.Type.CHECKING;
+import static br.thales.tools.transactions.manager.model.Account.Type.SAVING;
+import static br.thales.tools.transactions.manager.utils.Constants.Status.ACTIVE;
+import static org.springframework.http.HttpStatus.BAD_REQUEST;
 
 @RestController
 @RequestMapping("account")
@@ -14,14 +24,51 @@ public class AccountController {
 
     @Autowired
     AccountRepository accountRepository;
+    @Autowired
+    UserRepository userRepository;
 
-    @GetMapping
+    @GetMapping(value = "getAllAccounts")
     public List<Account> listAll(){
         return accountRepository.findAll();
     }
 
-    @PostMapping
-    public Account save(@RequestBody Account account){
-        return accountRepository.save(account);
+    @PostMapping(value = "getAccount")
+    public Account getAccount(@RequestBody Long id){
+        Optional<Account> account = accountRepository.findById(id);
+        if (account.isPresent()) {
+            return account.get();
+        } else {
+            throw new HttpClientErrorException(BAD_REQUEST, "Account not found");
+        }
+    }
+
+    @PostMapping(value = "addCheckingAccount")
+    public String addCheckingAccount(@RequestBody Long userId){
+        Optional<User> user = userRepository.findById(userId);
+        if(user.isEmpty()) {
+            throw new HttpClientErrorException(BAD_REQUEST, "User not found");
+        }
+        Account account = new Account();
+        account.setUserId(user.get().getId());
+        account.setStatus(ACTIVE);
+        account.setType(CHECKING);
+        account.setDate(new Date());
+        accountRepository.save(account);
+        return "Account ID: " + account.getId() + " for user " + user.get().getName();
+    }
+
+    @PostMapping(value = "addSavingAccount")
+    public String addSavingAccount(@RequestBody Long userId){
+        Optional<User> user = userRepository.findById(userId);
+        if(user.isEmpty()) {
+            throw new HttpClientErrorException(BAD_REQUEST, "User not found");
+        }
+        Account account = new Account();
+        account.setUserId(user.get().getId());
+        account.setStatus(ACTIVE);
+        account.setType(SAVING);
+        account.setDate(new Date());
+        accountRepository.save(account);
+        return "Account ID: " + account.getId() + " for user " + user.get().getName();
     }
 }
--- src/main/java/br/thales/tools/transactions/manager/model/Account.java
+++ src/main/java/br/thales/tools/transactions/manager/model/Account.java
@@ -1,27 +1,33 @@
 package br.thales.tools.transactions.manager.model;
 
+import br.thales.tools.transactions.manager.utils.Constants.*;
 import lombok.*;
 
 import javax.persistence.*;
 import java.util.Date;
-
 @Data
 @Builder
 @Entity
 @AllArgsConstructor
 @NoArgsConstructor
 public class Account {
+    public enum Type {
+        CHECKING,
+        SAVING
+    }
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     @NonNull
     private Long userId;
     @NonNull
-    private String type; // TODO: Change to enum or class
+    @Enumerated(EnumType.STRING)
+    private Type type;
     //NonNull
     private int version;
     @NonNull
-    private String status;
+    private Status status;
     @NonNull
     private Date date;
 }
